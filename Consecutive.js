45c994fd6998a1449fa122462595ebdb75e6012f
majadinjar <majadinjar@users.noreply.github.com>
2014-07-02 09:45:38 +0200

    Create Consecutive.js

---
+++ Consecutive.js
@@ -0,0 +1,29 @@
+function Consecutive(arr) { 
+  var arr = arr.sort(function(a,b) {return a-b;});
+  var results = [];
+  var number = 0;
+  for(var i = 0; i < arr.length-1; i++) {
+    var current = arr[i];
+    
+    var next = arr[i+1];
+    var diff = next - current;
+    console.log(diff);
+    var correct = diff - 1;
+    results.push(correct);
+    
+
+  }
+  
+  for(var x = 0; x < results.length; x++) {
+    number += results[x];
+  }
+  // code goes here
+  console.log(results);
+  return number; 
+         
+}
+   
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+Consecutive(readline());
